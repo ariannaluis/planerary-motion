6d0d45e9769b3c8f4f98c38c56aa42979a00e5d5
ariannaluis <ariannaluis@gmail.com>
2024-05-18 00:51:10 -0400

    added general and orbital constants

--- data/constants.py
+++ data/constants.py
@@ -1,5 +1,19 @@
-# gravitational constant
-# mass of sun (kg)
-# mass of earth (kg)
-# mass of jupiter (kg)
+# gravitational constant (m^3 kg^-1 s^-2)
+G = 6.6743e-11
 
+# masses (kg)
+M_SUN = 1.989e30
+M_EARTH = 5.9722e24
+M_JUPITER = 1.898e27
+
+# perihelion distances (m)
+PER_EARTH = 1.4709e8
+PER_JUPITER = 7.4059e8
+
+# aphelion distances (m)
+APH_EARTH = 1.521e8
+APH_JUPITER = 8.1636e8
+
+# orbital periods (s)
+ORBITAL_EARTH = 365.25 * 24 * 3600
+ORBITAL_JUPITER = 4332.59 * 24 * 3600
---
+++ src/formulas.py
@@ -0,0 +1,4 @@
+# calculate orbital parameters (Kepler's 2nd Law)
+    # semi-major axes
+    # semi-minor axes
+    # speeds at perihelion
