73b34383255cc7dbe7017dd48dd534d994f78ccd
ariannaluis <ariannaluis@gmail.com>
2024-05-18 02:02:08 -0400

    added data for constants and initial conditions

--- data/initial_conditions.py
+++ data/initial_conditions.py
@@ -1,11 +1,18 @@
-# Earth:
-    # Initial x position at perihelion (m)
-    # Initial y position (m)
-    # Initial x velocity (m/s)
-    # Initial y velocity at perihelion (m/s)
+import constants as c
+from src.formulas import vis_visa
 
-# Jupiter
-    # Initial x position at perihelion (m)
-    # Initial y position (m)
-    # Initial x velocity (m/s)
-    # Initial y velocity at perihelion (m/s)
+# initial x position at perihelion (m)
+X0_EARTH = c.PER_EARTH
+X0_JUPITER = c.PER_JUPITER
+
+# initial y position (m)
+Y0_EARTH = 0
+Y0_JUPITER = 0
+
+# initial x velocity (m/s)
+VX0_EARTH = 0
+VX0_JUPITER = 0
+
+# initial y velocity at perihelion (m/s)
+VY0_EARTH = vis_visa(c.M_SUN, c.PER_EARTH, c.SEMIMAJOR_EARTH)
+VY0_JUPITER = vis_visa(c.M_SUN, c.PER_JUPITER, c.SEMIMAJOR_JUPITER)
