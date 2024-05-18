d9b587be991e2f5fd4a48fc07588cd4361c92c4e
ariannaluis <ariannaluis@gmail.com>
2024-05-17 22:44:47 -0400

    initial files

---
+++ data/constants.py
@@ -0,0 +1,5 @@
+# gravitational constant
+# mass of sun (kg)
+# mass of earth (kg)
+# mass of jupiter (kg)
+
---
+++ data/initial_conditions.py
@@ -0,0 +1,11 @@
+# Earth:
+    # Initial x position at perihelion (m)
+    # Initial y position (m)
+    # Initial x velocity (m/s)
+    # Initial y velocity at perihelion (m/s)
+
+# Jupiter
+    # Initial x position at perihelion (m)
+    # Initial y position (m)
+    # Initial x velocity (m/s)
+    # Initial y velocity at perihelion (m/s)
